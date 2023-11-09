c4455e677249cddda8e0915b12abba09066eaf8d
anurag96ar <anu.rameshar007@gmail.com>
2023-11-09 18:38:42 +0530

    last

--- server/index.js
+++ server/index.js
@@ -38,8 +38,8 @@ import http from "http"
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000", "https://worklink.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  req.header("Access-Control-Allow-Origin", "http://localhost:3000", "https://worklink.vercel.app");
+  req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
